8314db12b8618edab69338b63425ca2a7e2f4ccc
jamesssmith1324 <jamesssmith1324@gmail.com>
2019-11-15 10:12:30 +0000

    reset button added and code refactored

--- js/fruit.js
+++ js/fruit.js
@@ -17,6 +17,9 @@ let incBetBtn = document.getElementById("betBtn");
 let betInp = document.getElementById("incBetInp");
 let modal = document.getElementById("modWrap");
 let modalBtn = document.getElementById("modalClose");
+let final = [];
+let resetBtn = document.getElementById("resetBtn");
+
 
 const increaseBet = () =>{
     bet = bet + 2;
@@ -32,14 +35,20 @@ function winner(){
     balance += bet*winOrder;
 }
 
+
 function spin(){
-    balance = balance - bet
-    //alert(bet)
-    let final = []
+    balance -= bet;
+
     final.push(spinReel("r1"))
     final.push(spinReel("r2"))
     final.push(spinReel("r3"))
     //Check if the reels match up by checking 1 and 0 & 0 and 2
+    
+    winCheck();
+
+}    
+
+let winCheck = () => {
     if (final[0] == final[1] && final[0] == final[2]){
         winner()
     }
@@ -47,39 +56,33 @@ function spin(){
         loser()
     }
     update_theScrene();
+}
 
-}    
 function spinReel(reel){
-    let i = Math.floor(Math.random()*6)
-    if (i == 0){
+    winOrder = Math.floor(Math.random()*6) +1;
+    if (winOrder == 1){
         document.getElementById(reel).src = "images/Cherry.png"
-        winOrder = 1;
     }
-    if (i == 1){
+    if (winOrder == 2){
         document.getElementById(reel).src = "images/Grapes.png"
-        winOrder = 2;
     }
-    if (i == 2){
+    if (winOrder == 3){
         document.getElementById(reel).src = "images/Lemon.png"
-        winOrder = 3;
     }
-    if (i == 3){
-        document.getElementById(reel).src = "images/Orange.png"
-        winOrder = 6;
-    }
-    if (i == 4){
-        document.getElementById(reel).src = "images/Strawberry.png"
-        winOrder = 5;
-    }
-    if (i == 5){
+    if (winOrder == 4){
         document.getElementById(reel).src = "images/Watermelon.png"
-        winOrder = 4;
     }
-    return i
+    if (winOrder == 5){
+        document.getElementById(reel).src = "images/Strawberry.png"
+    }
+    if (winOrder == 6){
+        document.getElementById(reel).src = "images/Orange.png"
+    }
+    return winOrder;
 }
 
 const update_theScrene = () => {
-    document.getElementById("balanceDisplay").innerHTML = balance
+    document.getElementById("balanceDisplay").innerHTML = balance;
 
 }
 
@@ -91,12 +94,15 @@ incBetBtn.addEventListener('click', () => {
     } else{
         betInp.value = null;
         betInp.placeholder = "Please enter 1-100";
-        
     }
 })
 
 modalBtn.addEventListener('click', () => {
     modal.style.display = "none";
+})
+
+resetBtn.addEventListener('click', () => {
+    window.location.reload();
 })
 
 update_theScrene();
